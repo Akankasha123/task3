4c86ed4dd7075c5cb8d6f73039e619384de94aa1
Akankasha123 <162999790+Akankasha123@users.noreply.github.com>
2024-03-11 18:51:55 +0530

    Add files via upload

---
+++ BMI.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # BMI CALCULATOR
+
+# In[3]:
+
+
+Height = float(input("Enter the height in cm: "))  
+Weight = float(input("Enter the weight in kg: "))  
+# defining a function for BMI  
+The_BMI = Weight / (Height/100)**2  
+# printing the BMI  
+print("Your Body Mass Index is", The_BMI)  
+# using the if-elif-else conditions  
+if The_BMI <= 18.5:  
+    print("Oops! You are underweight.")  
+elif The_BMI <= 24.9:  
+    print("Awesome! You are healthy.")  
+elif The_BMI <= 29.9:  
+    print("Eee! You are over weight.")  
+else:  
+    print("Seesh! You are obese.")  
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# 
+
+# In[2]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
